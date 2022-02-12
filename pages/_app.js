c34531ec77fd0a9b36b713ee1f7c2bd88536d376
subhasis4502 <subhasis4502@gmail.com>
2022-02-12 10:50:41 +0530

    SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import "../styles/globals.css";
-import NextApp from "next/app";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Component {...pageProps} />
-    );
-  }
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
