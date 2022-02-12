eafa2673831b7dd5b33bdb6bec7267edf7eef627
subhasis4502 <subhasis4502@gmail.com>
2022-02-12 13:35:14 +0530

    responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import * as React from "react";
-import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Typography from "@mui/material/Typography";
 import styles from "../styles/Home.module.css";
 import { Grid } from "@mui/material";
 import React from "react";
-import theme from "../src/theme";
 
 export default function Index() {
   // console.log("hello")
